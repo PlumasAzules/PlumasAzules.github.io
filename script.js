22130f9355118c09795786fd565755bea653114c
PlumasAzules <80261787+PlumasAzules@users.noreply.github.com>
2021-03-21 18:58:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var x6Ic_p = [":)","mundo","quieren","pan","si",undefined];
+var a; var b = x6Ic_p;
+ a == b[5]? console.log(b[0]) : console.log(false);
+ var jj= document.createElement("h1");
+ var txt = document.createTextNode("ConsoleLog(1920)");
+ jj.appendChild(txt);
+document.getElementById("div");
+document.body.insertBefore(jj,div);
+var txtt = document.createTextNode("Good job! 2nd step done. Now is time to change the DOM. Start with something easy: change the h1 to the title content ;)");
+jj.id = "ww";
+function ConsoleLog(n) {
+  if (n === 1920) {
+    jj.style.color="#FFFFFF"
+    jj.style.fontSize="20px"
+    jj.appendChild(txtt);
+    document.getElementById("div");
+    document.body.insertBefore(jj,div);
+    jj.removeChild(txt)
+  }else if (typeof n === "number" && n!==1920){
+    console.log("Hmmm hot, hot, but not at all")
+  }else if (n === "1920" && n!==1920){
+    console.log("We don't like strings")
+  }else{
+    console.log("Hmmm cold, cold...")
+  }
+}
+
+/////Zft, zft, uijt jt uif nptu XFU dpef fwfs cvu... nbzcf uibu't uif pckfdujwf ;)
